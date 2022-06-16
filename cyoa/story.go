79acd28fc78c1d3ecb5f25213082dc57832a0120
Gera <gerardo.aguilar.calderon@gmail.com>
2022-06-16 00:10:20 -0500

    modify: Adding styles to html template

--- cyoa/story.go
+++ cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 var tpl *template.Template
@@ -23,16 +25,65 @@ var defaultHandlerImpl = `
   <title>Choose your own adventure</title>
 </head>
 <body>
-  <h1>{{.Title}}</h1>
-  {{range .Paragraphs}}
-    <p>{{.}}</p>
-  {{end}}
+	<section class="page">
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
 
-  <ul>
-    {{range .Options}}
-      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
-    {{end}}
-  </ul>
+		<ul>
+			{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</section>
+	<style>
+		body {
+			font-family: helvetica, arial;
+		}
+
+		h1 {
+			text-align: center;
+			position: relative;
+		}
+
+		.page {
+			width: 80%;
+			max-width: 500px;
+			margin: auto;
+			margin-top: 40px;
+			margin-bottom: 40px;
+			padding: 80px;
+			background: #FFFCF6;
+			border: 1px solid #eee;
+			box-shadow: 0 10px 6px -6px #777;
+		}
+
+		ul {
+			border-top: 1px dotted #ccc;
+			padding: 10px 0 0 0;
+			-webkit-padding-start: 0;
+		}
+
+		li {
+			padding-top: 10px;
+		}
+
+		a,
+		a:visited {
+			text-decoration: none;
+			color: #6295b5;
+		}
+
+		a:active,
+		a:hover {
+			color: #7792a2;
+		}
+
+		p {
+			text-indent: 1em;
+		}
+	</style>
 </body>
 </html>
 `
@@ -46,10 +97,26 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
+	path := strings.TrimSpace(r.URL.Path)
+
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+
+	path = path[1:]
+
+	var chapter Chapter
+	var ok bool
+
+	if chapter, ok = h.s[path]; !ok {
+		http.Error(w, "Chapter not found", http.StatusNotFound)
+	}
+
+	err := tpl.Execute(w, chapter)
 
 	if err != nil {
-		panic(err)
+		log.Printf("%v", err)
+		http.Error(w, "Something went wrong...", http.StatusInternalServerError)
 	}
 
 }
