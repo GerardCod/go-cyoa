6a93f96b7496da0e2196006adee975216e8a0863
Gera <gerardo.aguilar.calderon@gmail.com>
2022-06-15 23:36:45 -0500

    Changes made: - modify: changing app folder name to cmd - modify: adding template processing to story.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/GerardCod/go-adventure/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "The port to start the CYOA web application on")
 	file := flag.String("file", "gopher.json", "The json file with the CYOA story")
-
 	flag.Parse()
 
 	fmt.Printf("Using the story in %s.\n", *file)
 
 	content, err := os.Open(*file)
-
 	if err != nil {
 		panic(err)
 	}
 
-	var story cyoa.Story
-
-	d := json.NewDecoder(content)
-
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(content)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d.\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- cyoa/story.go
+++ cyoa/story.go
@@ -1,5 +1,71 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerImpl))
+}
+
+var defaultHandlerImpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Choose your own adventure</title>
+</head>
+<body>
+  <h1>{{.Title}}</h1>
+  {{range .Paragraphs}}
+    <p>{{.}}</p>
+  {{end}}
+
+  <ul>
+    {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+  </ul>
+</body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	var story Story
+
+	d := json.NewDecoder(r)
+
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+
+	return story, nil
+}
+
 // Story represents the content for the cyoa content
 type Story map[string]Chapter
 
