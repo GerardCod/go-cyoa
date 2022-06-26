28060679c0c14c2a56961069fb291f4a7ee48c9f
Gera <gerardo.aguilar.calderon@gmail.com>
2022-06-26 17:40:13 -0500

    modify: Finishing the project of choose your own adventure

--- cyoa/story.go
+++ cyoa/story.go
@@ -88,22 +88,48 @@ var defaultHandlerImpl = `
 </html>
 `
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+
+	for _, opt := range opts {
+		opt(&h)
+	}
+
+	return h
 }
 
 type handler struct {
-	s Story
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 
-	path = path[1:]
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 
 	var chapter Chapter
 	var ok bool
@@ -112,7 +138,7 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Chapter not found", http.StatusNotFound)
 	}
 
-	err := tpl.Execute(w, chapter)
+	err := h.t.Execute(w, chapter)
 
 	if err != nil {
 		log.Printf("%v", err)
